f486aada68e2bc356620ea61d13b29228723e870
iAmBalanceAR <arkansasdj@gmail.com>
2025-01-21 17:21:59 -0600

    safety commit before testing new LLM

--- check_lead_function.sql
+++
@@ -1,8 +0,0 @@
--- Get the current definition of get_lead_with_details function
-SELECT 
-    pg_get_functiondef(oid) as definition
-FROM 
-    pg_proc 
-WHERE 
-    proname = 'get_lead_with_details'
-    AND pronamespace = (SELECT oid FROM pg_namespace WHERE nspname = 'public'); 
